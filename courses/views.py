84befda5d8795e0fff370f59f11c1aafb0742467
Ianadika44 <adika19ian@gmail.com>
2020-06-10 14:56:41 +0300

    started the workflow

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime as dt
 
 # Create your views here.
+
+
+def welcome(request):
+    return HttpResponse('Welcome to the Flash-Card')
+
+
+def notes_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1>Notes for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday', 'Tuesday', 'Wednesday',
+            'Thursday', 'Friday', 'Saturday', "Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
