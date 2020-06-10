6e3bec01b0b1421f9bc656632c24fb8c58197912
Ianadika44 <adika19ian@gmail.com>
2020-06-11 00:12:49 +0300

    styled the navbar

--- courses/urls.py
+++ courses/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    
+    url('^today/$',views.notes_of_day,name='notesToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_notes,name = 'pastNotes') 
 ]
