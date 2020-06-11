b54676b83c3acdc7e8f5920a79e345b24fab923e
hukish <hudsonhukish@gmail.com>
2020-06-11 19:58:20 +0300

    added form file

--- courses/views.py
+++ courses/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 import datetime as dt
 from .models import Cards,Profile
+from .forms import CardForm,ProfileForm
 
 # Create your views here.
 
